48d2dea7ab535fa1c23f782ee59d3545b08ab116
emadhegazy <acc.ekh1@gmail.com>
2023-06-15 06:40:22 -0700

    my first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - enty point
+ *
+ *Description:A C programing that print useing prin ffunction.
+ *
+ *return: alwats 0 (success)
+*/
+
+int main(void)
+{
+       printf("with proper grammar, but the outcome is a piece of art,/n");
+       return (0);
+}
