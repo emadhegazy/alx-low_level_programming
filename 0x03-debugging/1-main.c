3d8c3aa5cdacc07a4dcf770917c14f8ccdcc5013
emadhegazy <acc.ekh1@gmail.com>
2023-06-22 12:07:21 -0700

    done

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,7 +6,9 @@
 */
 
 int main(void)
+
 {
+
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
@@ -14,6 +16,9 @@ i = 0;
 /*{*/
 /*putchar(i);*/
 /*}*/
+
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
+
 }
