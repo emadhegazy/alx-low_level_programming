7d24a6fcc104c71bdf9d3153e146aef524595be1
emadhegazy <acc.ekh1@gmail.com>
2023-07-07 10:24:25 -0700

    done

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,15 @@
 #include "main.h"
+#include<stdio.h>
 /**
-* _puts - prints a string, followed by a new line, to stdout
-* @str: string to print
-*/
-void _puts(char *str)
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+void _putts(char *str)
 {
-while (*str != '\0')
-{
-putchar(*str++);
+        while (*str != '\0')
+        {
+                putchar(*str++);
+        }
+                putchar('\n');
 }
-putchar('\n');
-}
+
