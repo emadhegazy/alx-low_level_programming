8af660d084cced50547824a98698c65c735fe8ab
emadhegazy <acc.ekh1@gmail.com>
2023-07-11 06:24:33 -0700

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	s2 = "";
 	i = 0;
 	ai = 0;
-	while (si[i] != '\0')
+	while (s1[i] != '\0')
 	i++;
 	while (s2[ai] != '\0')
 	ai++;
@@ -26,7 +26,7 @@ char *str_concat(char *s1, char *s2)
 	return (NULL);
 	i = 0;
 	ai = 0;
-	while (si[i] != '\0')
+	while (s1[i] != '\0')
 	{
 	concat[i] = s1[i];
 	i++;
