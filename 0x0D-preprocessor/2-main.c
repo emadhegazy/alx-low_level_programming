f1a5aba87fa6b99f1ab61e3bf52959f954f47b42
emadhegazy <acc.ekh1@gmail.com>
2023-07-17 04:25:56 -0700

    done

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print name of this sourc file.
+ * main - prints the name of the file
  *
- * Return always 0.
- *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
+
