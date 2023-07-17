10b952ac071bab990daa95339e63f18863af1433
emadhegazy <acc.ekh1@gmail.com>
2023-07-17 03:33:20 -0700

    done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * descriptipn:just a lone dog struct
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+#endif
+
