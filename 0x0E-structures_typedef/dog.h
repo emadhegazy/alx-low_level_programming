4991d88d65266b6f159b0fb554cb834697a10b14
emadhegazy <acc.ekh1@gmail.com>
2023-07-17 04:16:58 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,7 @@ char *owner;
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
 
